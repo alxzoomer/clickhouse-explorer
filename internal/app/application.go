c0b6df8c3f007deb7ddf82a774bd422606e102f9
Alexey Nikitin <alxzoomer@gmail.com>
2021-12-31 01:09:54 +0400

    feat: add example http server
    Add http server to serve single http GET endpoint.

--- internal/app/application.go
+++ internal/app/application.go
@@ -2,10 +2,10 @@ package app
 
 import (
 	"database/sql"
-	"fmt"
 	"github.com/ClickHouse/clickhouse-go"
 	"github.com/alxzoomer/clickhouse-explorer/pkg/dbexport"
 	"log"
+	"net/http"
 )
 
 type Application struct {
@@ -16,32 +16,54 @@ func New() *Application {
 }
 
 func (app *Application) Run() {
-	fmt.Println("Starting clickhouse-explorer")
+	log.Println("Starting clickhouse-explorer. Open http://localhost:8000")
+	http.HandleFunc("/", handler) // each request calls handler
+	log.Fatal(http.ListenAndServe("localhost:8000", nil))
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		return
+	}
+	if r.RequestURI != "/" {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+	j, err := queryExample()
+	if err != nil {
+		if exception, ok := err.(*clickhouse.Exception); ok {
+			log.Printf("[%d] %s \n%s\n", exception.Code, exception.Message, exception.StackTrace)
+		} else {
+			log.Println(err)
+		}
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	_, err = w.Write(j)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+}
+
+func queryExample() ([]byte, error) {
 	// clickhouseUrl := "tcp://127.0.0.1:9000?debug=true"
 	clickhouseUrl := "tcp://127.0.0.1:9000?debug=false"
 	connect, err := sql.Open("clickhouse", clickhouseUrl)
 	if err != nil {
-		log.Fatal(err)
+		return nil, err
 	}
 	defer connect.Close()
 	if err := connect.Ping(); err != nil {
-		if exception, ok := err.(*clickhouse.Exception); ok {
-			fmt.Printf("[%d] %s \n%s\n", exception.Code, exception.Message, exception.StackTrace)
-		} else {
-			fmt.Println(err)
-		}
-		return
+		return nil, err
 	}
 
 	rows, err := connect.Query("SELECT * FROM test.example_table")
 	if err != nil {
-		log.Fatal(err)
+		return nil, err
 	}
 	defer rows.Close()
 
-	js, err := dbexport.MarshalDbRows(rows)
-	if err != nil {
-		log.Fatal(err)
-	}
-	log.Printf("%s", string(js))
+	return dbexport.MarshalDbRows(rows)
 }
