6a0d50614149ae4849aa8c62f61fd8658c1afe12
Alexey Nikitin <alxzoomer@gmail.com>
2021-12-31 00:29:21 +0400

    refactor: move app logic into app package
    Move app login from main into app package.

--- cmd/clickhouse-explorer/main.go
+++ cmd/clickhouse-explorer/main.go
@@ -1,40 +1,10 @@
 package main
 
 import (
-	"database/sql"
-	"fmt"
-	"github.com/ClickHouse/clickhouse-go"
-	"github.com/alxzoomer/clickhouse-explorer/pkg/dbexport"
-	"log"
+	"github.com/alxzoomer/clickhouse-explorer/internal/app"
 )
 
 func main() {
-	fmt.Println("Starting clickhouse-explorer")
-	// clickhouseUrl := "tcp://127.0.0.1:9000?debug=true"
-	clickhouseUrl := "tcp://127.0.0.1:9000?debug=false"
-	connect, err := sql.Open("clickhouse", clickhouseUrl)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer connect.Close()
-	if err := connect.Ping(); err != nil {
-		if exception, ok := err.(*clickhouse.Exception); ok {
-			fmt.Printf("[%d] %s \n%s\n", exception.Code, exception.Message, exception.StackTrace)
-		} else {
-			fmt.Println(err)
-		}
-		return
-	}
-
-	rows, err := connect.Query("SELECT * FROM test.example_table")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer rows.Close()
-
-	js, err := dbexport.MarshalDbRows(rows)
-	if err != nil {
-		log.Fatal(err)
-	}
-	log.Printf("%s", string(js))
+	a := app.New()
+	a.Run()
 }
